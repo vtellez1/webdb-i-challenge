39789c740743e785154c226dde7bc2a05309dd56
Vanessa <vanessatellez45@gmail.com>
2020-01-19 18:32:42 -0500

    set up for Accounts API

---
+++ accounts/account-router.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        
+    } catch (error){
+        next(error)
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try{
+
+    } catch (error){
+        next(error)
+    }
+});
+
+router.post('/', async (req, res) => {
+    try{
+
+    } catch (error){
+        next(error)
+    }
+});
+
+router.put('/', async (req, res) => {
+    try{
+
+    } catch (error){
+        next(error)
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+
+    } catch (error){
+        next(error)
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const AccountRouter = require('./accounts/account-router.js');
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h3> Web DB I Challenge </h3>');
+});
 
 module.exports = server;
