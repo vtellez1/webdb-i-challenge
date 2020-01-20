8cbfafcfafdb2080bbe10a38ee8601504bc80f6e
Vanessa <vanessatellez45@gmail.com>
2020-01-20 09:35:55 -0500

    wrote endpoints but not showing correctly yet

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -1,44 +1,78 @@
 const express = require('express');
 
+// database access using knex
 const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try{
-        
+        res.json(await db.select("*").from("accounts"))
+    } catch (err){
+        next(err)
+    }
+    // db.select("*").from("accounts")
+    // .then(accounts => {
+    //     res.status(200).json(accounts)
+    // })
+    // .catch(error =>{
+    //     console.log(error);
+    //     res.status(500).json({
+    //         errorMessage: "Error getting accounts"
+    //     })
+    // })
+});
+
+router.get('/:id', async (req, res, next) => {
+    try{
+        const account = await db("accounts").where("id",
+        req.params.id).first()
+        res.json(account)
+    } catch (error){
+        next(error)
+    }
+    // db.select("*").from("accounts").where({ id: req.params.id }).first()
+    // .then(account => {
+    //     res.status(200).json(account);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    //     res.status(500).json({
+    //     errorMessage: "Error getting account"
+    // })
+});
+
+router.post('/', async (req, res, next) => {
+    try{
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget,
+        }
+
+        const [id] = await db("accounts").insert(payload)
+        res.json(await db("accounts").where("id", id).first())
     } catch (error){
         next(error)
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.put('/', async (req, res, next) => {
     try{
-
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget,
+        }
+        await db("account").where("id", req.params.id).update(payload)
+        res.json( await db("accounts").where("id", req.params.id).first())
     } catch (error){
         next(error)
     }
 });
 
-router.post('/', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try{
-
-    } catch (error){
-        next(error)
-    }
-});
-
-router.put('/', async (req, res) => {
-    try{
-
-    } catch (error){
-        next(error)
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try{
-
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end()
     } catch (error){
         next(error)
     }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const AccountRouter = require('./accounts/account-router.js');
 
-const db = require('./data/dbConfig.js');
+//const db = require('./data/dbConfig.js');
 
 const server = express();
 
@@ -12,6 +12,13 @@ server.use('/api/accounts', AccountRouter);
 
 server.get('/', (req, res) => {
     res.send('<h3> Web DB I Challenge </h3>');
+});
+
+server.use((err, req, res) => {
+    console.log(err)
+    res.status(500).json({ 
+        message: "Something went wrong."
+    });
 });
 
 module.exports = server;
